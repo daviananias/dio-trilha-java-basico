9b91a02edf7ad04d01afc7452a5356ad188423ed
Davi <daviananias2@hotmail.com>
2022-10-24 23:30:11 -0300

    criacão de operadores2.java

---
+++ operadores/src/Operadores2.java
@@ -0,0 +1,21 @@
+public class Operadores2 {
+    public static void main(String[] args) {
+        String concatenacao = "?";
+
+        concatenacao = 1 + 1 + 1 + "1";
+        System.out.println(concatenacao);
+
+        concatenacao = 1 + "1" + 1 + 1;
+        System.out.println(concatenacao);
+
+        concatenacao = 1 + "1" + 1 + "1";
+        System.out.println(concatenacao);
+
+        concatenacao = "1" + 1 + 1 + 1;
+        System.out.println(concatenacao);
+
+        concatenacao = "1" + (1 + 1 + 1);
+        System.out.println(concatenacao);
+
+    }
+}
