842d18739120bc52a1459da23f209ef74faf0f40
Davi <daviananias2@hotmail.com>
2022-10-25 22:36:57 -0300

    criação de Operadores5.java

---
+++ operadores/src/Operadores5.java
@@ -0,0 +1,11 @@
+public class Operadores5 {
+    public static void main(String[] args) {
+        int a, b;
+        a = 6;
+        b = 4;
+
+        int resultado = a == b ? 1 : 0;
+
+        System.out.println(resultado);
+    }
+}
