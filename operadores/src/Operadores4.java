298adbf2b7ed9d3dcf3cee1000e67287fdaac4ea
Davi <daviananias2@hotmail.com>
2022-10-25 22:21:53 -0300

    criação de Operadores4.java

---
+++ operadores/src/Operadores4.java
@@ -0,0 +1,9 @@
+public class Operadores4 {
+    public static void main(String[] args) {
+        boolean variavel = true;
+
+        variavel = !variavel;
+
+        System.out.println(variavel);
+    }
+}
