65e20fe41b24423171e9a40ad025c7d540380988
Davi <daviananias2@hotmail.com>
2022-10-26 21:07:43 -0300

    criação de operadores 6 e 7

---
+++ operadores/src/Operadores6.java
@@ -0,0 +1,29 @@
+public class Operadores6 {
+    public static void main(String[] args) {
+        String nomeUm = "Davi";
+        String nomeDois = new String("Davi");
+
+        System.out.println(nomeUm.equals(nomeDois));
+
+        int numero1 = 1;
+        int numero2 = 2;
+
+        boolean simNao = numero1 == numero2;
+
+        if (numero1 == numero2) {
+            System.out.println("a nossa condição é verdadeira");
+
+        }
+
+        System.out.println("numeroUm é igual a numeroDois? " + simNao);
+
+        simNao = numero1 != numero2;
+
+        System.out.println("numeroUm é difernte de numeroDois? " + simNao);
+
+        simNao = numero1 > numero2;
+
+        System.out.println("numeroUm é maior que numeroDois? " + simNao);
+
+    }
+}
